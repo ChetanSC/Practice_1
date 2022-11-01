3cf9dc4f30b39e58201fa96c625c027c5e2d27fa
Chetan Prajapati <chetanprajapati.cp1246@gmail.com>
2022-11-01 12:15:49 +0530

    add congig

---
+++ Config/Configdata.py
@@ -0,0 +1,16 @@
+# valid
+correct_email = "chetanprajapati.cp1246@gmail.com"
+correct_passwd = "Chet@n143"
+
+# invalid
+incorrect_email = "chetn1245@gmail.com"
+incorrect_passwd = "chetan76"
+
+# create
+email_field_in_forget_password = "chetanprajapati.cp1246@gmail.com"
+
+empty_email = ""
+empty_passwd = ""
+
+dress1 = "dress"
+p_dress = '//div[@id="center_column"]//img[@src="http://automationpractice.com/img/p/1/0/10-home_default.jpg"]'
