37d3d2178f0799edc5719b424ad0e4547c3d4ad8
Chetan Prajapati <chetanprajapati.cp1246@gmail.com>
2022-11-04 11:13:10 +0530

    update

--- Pages/OrderLocators.py
+++ Pages/OrderLocators.py
@@ -5,7 +5,7 @@ add_to_cart = '//p[@id="add_to_cart"]//span[text()="Add to cart"]'
 proceed_to_checkout = "a.btn.btn-default.button.button-medium"
 proceed_to_checkout_in_summary = "a.button.btn.btn-default.standard-checkout.button-medium"
 proceed_to_checkout_in_address = "button.button.btn.btn-default.button-medium"
-teams_of_service = "#cgv"
+teams_of_service = "input#cgv"
 proceed_to_checkout_in_shipping = "button.button.btn.btn-default.standard-checkout.button-medium"
 payment_via_cheque = "a.cheque"
 confirm_my_order = "button.button.btn.btn-default.button-medium"
--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
 
 @pytest.fixture()
 def initiate_driver():
